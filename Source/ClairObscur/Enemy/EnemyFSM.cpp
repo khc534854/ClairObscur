565cbc3efec461671c76e888e97cf444deaeefde
kleo19 <kleo19@naver.com>
2025-09-09 15:22:50 +0900

    EnemyEnumWIP

---
+++ Source/ClairObscur/Enemy/EnemyFSM.cpp
@@ -0,0 +1,77 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyFSM.h"
+
+#include "Enemy.h"
+
+
+// Sets default values for this component's properties
+UEnemyFSM::UEnemyFSM()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UEnemyFSM::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 소유자 찾기
+	me = Cast<AEnemy>(GetOwner());
+	// 타겟 찾기
+	//target = Cast<ATPSPlayer>(UGameplayStatics::GetActorOfClass(GetWorld(), ATPSPlayer::StaticClass()));	
+}
+
+
+// Called every frame
+void UEnemyFSM::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// 현재 상태 출력
+	UE_LOG(LogTemp, Log, TEXT("State: %s"), *UEnum::GetValueAsString(_state));
+
+
+	switch (_state)
+	{
+	case EEnemyState::Idle:
+		IdleState();
+		break;
+	case EEnemyState::Attack:
+		AttackState();
+		break;
+	case EEnemyState::Damage:
+		DamageState();
+		break;
+	case EEnemyState::Die:
+		DieState();
+		break;
+	}
+	
+}
+
+void UEnemyFSM::IdleState()
+{
+}
+
+void UEnemyFSM::AttackState()
+{
+}
+
+void UEnemyFSM::DamageState()
+{
+}
+
+void UEnemyFSM::DieState()
+{
+}
+
+void UEnemyFSM::OnDamageProcess(FVector hitDirection)
+{
+}
